cf35a41bea96d2e67fd2d66aa285b1c5291b9f15
Vechniy Rabotnik <111284399+VechniyRabotnik@users.noreply.github.com>
2025-08-12 21:34:29 +0300

    Update
    Unification of the image

--- main.go
+++ main.go
@@ -4,34 +4,65 @@ import (
 	"fmt"
 	"image"
 	"image/color"
-	"image/jpeg"
+	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/nfnt/resize"
+
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 )
 
 var asciiChars = []byte(" .:-=+*#%@")
 
 func main() {
-	// Открываем изображение
-	file, err := os.Open("input.jpg")
+	filenameWithoutExt := "input"
+	dir := "."
+
+	entries, err := os.ReadDir(dir)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+
+	var foundFile string
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			name := entry.Name()
+			ext := filepath.Ext(name)
+			base := name[:len(name)-len(ext)]
+			if base == filenameWithoutExt {
+				foundFile = filepath.Join(dir, name)
+				break
+			}
+		}
+	}
+
+	if foundFile == "" {
+		fmt.Println("Файл не найден")
+		return
+	}
+
+	fmt.Println("Нашли файл:", foundFile)
+
+	file, err := os.Open(foundFile)
+	if err != nil {
+		log.Fatal(err)
 	}
 	defer file.Close()
 
-	img, err := jpeg.Decode(file)
+	img, _, err := image.Decode(file)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	// Меняем размер
 	newWidth := 80
 	ratio := float64(img.Bounds().Dy()) / float64(img.Bounds().Dx())
-	newHeight := uint(float64(newWidth) * ratio * 0.55) // коррекция пропорций
+	newHeight := uint(float64(newWidth) * ratio * 0.55)
+
 	resized := resize.Resize(uint(newWidth), newHeight, img, resize.Lanczos3)
 
-	// Генерируем
 	generateASCII(resized)
 }
 
